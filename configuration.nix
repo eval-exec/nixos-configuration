deaddaebf6ba7b4383ef07af25ab6a430b1fd084
Eval EXEC <execvy@gmail.com>
2023-04-30 11:16:02 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -229,6 +229,10 @@
     clash-meta
   ];
 
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   nix.settings.substituters = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://nix-community.cachix.org"
