deb7b7ce3a1697a473738ca2b51a57b77a10134b
Eval EXEC <execvy@gmail.com>
2023-05-03 11:00:55 +0800

    add trust keys

--- configuration.nix
+++ configuration.nix
@@ -250,13 +250,16 @@
   nix.settings.trusted-users = ["root" "exec"];
 
   nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
   ];
 
   nix.settings.substituters = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
+    "https://nix-community.cachix.org"
+    "https://devenv.cachix.org"
   ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
