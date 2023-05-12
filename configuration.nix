0806a5fd8ce8f8096cfdcfe2913b68ab9836b60d
Eval EXEC <execvy@gmail.com>
2023-05-12 18:35:58 +0800

    add rime
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,9 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
+      fcitx5-rime
       fcitx5-chinese-addons
+      fcitx5-with-addons
       fcitx5-configtool
       fcitx5-gtk
     ];
