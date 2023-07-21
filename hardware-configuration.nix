58611c876b67538a3cde88c32248f4fb2dfe7e69
Eval EXEC <execvy@gmail.com>
2023-07-21 20:56:30 +0800

    increase boot kernel loglevel

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,6 +4,7 @@
 { config, lib, pkgs, modulesPath, ... }: {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.consoleLogLevel = 7;
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
