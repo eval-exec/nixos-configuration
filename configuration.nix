8bc5e88017bd4ab16bb227521db0feb34f339358
Eval EXEC <execvy@gmail.com>
2023-12-16 20:29:11 +0800

    update substituteers

--- configuration.nix
+++ configuration.nix
@@ -521,7 +521,7 @@
       ];
 
       substituters = [
-        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://devenv.cachix.org"
