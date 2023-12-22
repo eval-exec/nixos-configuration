5707b1edda8d1e75ddddb1d51ca2b2befa16df5f
Eval EXEC <execvy@gmail.com>
2023-12-22 16:18:21 +0800

    update

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     hardware.url = "github:nixos/nixos-hardware";
-    nur.url = github:nix-community/NUR;
-
+    nur.url = "github:nix-community/NUR";
 
     flake-utils = { url = "github:numtide/flake-utils"; };
 
@@ -29,34 +28,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, nur, home-manager, emacs-overlay, sops-nix, ... }: {
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs =
+    { self, nixpkgs, nur, home-manager, emacs-overlay, sops-nix, ... }: {
+      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    # packages.x86_64-linux = [
-    # ];
+      # packages.x86_64-linux = [
+      # ];
 
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-    nixosConfigurations.Mufasa = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit nixpkgs home-manager emacs-overlay sops-nix; };
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-        nur.nixosModules.nur
-        sops-nix.nixosModules.sops
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.exec = import ./home.nix;
-          # home-manager.extraSpecialArgs = { inherit config; };
-          nixpkgs.overlays = [ emacs-overlay.overlay ];
+      nixosConfigurations.Mufasa = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit nixpkgs home-manager emacs-overlay sops-nix; };
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          nur.nixosModules.nur
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.exec = import ./home.nix;
+            # home-manager.extraSpecialArgs = { inherit config; };
+            nixpkgs.overlays = [ emacs-overlay.overlay ];
 
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
-      ];
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
     };
-  };
 }
