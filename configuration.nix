08aedb52ddbcd7bc8330d74ee06b1340e55956b2
Eval EXEC <execvy@gmail.com>
2024-02-24 21:23:09 +0800

    enable amdgpu

--- configuration.nix
+++ configuration.nix
@@ -216,7 +216,7 @@
         };
       };
 
-      videoDrivers = [ "nvidia" ];
+      videoDrivers = [ "amdgpu" ];
       #   config = lib.mkAfter ''
       #     Section "Module"
       #         Load           "modesetting"
