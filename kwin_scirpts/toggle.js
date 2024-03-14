0e1a3b679da185c4b265db773431909068696775
Eval EXEC <execvy@gmail.com>
2024-03-14 18:09:34 +0800

    nix flake update

--- kwin_scirpts/toggle.js
+++ kwin_scirpts/toggle.js
@@ -12,14 +12,14 @@ function toggleClient(app_name) {
     );
   };
 
-  console.log("in toggle client", app_name);
+  // console.log("in toggle client", app_name);
   let clients = workspace.windowList();
 
   let client = null;
 
   for (var i = 0; i < clients.length; i++) {
     let resource_name = clients[i].resourceName;
-    console.log(resource_name);
+    // console.log(resource_name);
 
     if (resource_name == app_name) {
       client = clients[i];
@@ -57,20 +57,20 @@ function toggleClient(app_name) {
       // client.desktop = workspace.currentDesktop.id;
       // client.minimized = false;
       workspace.activeWindow = client;
-      console.log("app not in current desktop, activate now", app_name);
+      // console.log("app not in current desktop, activate now", app_name);
     } else {
       if (client.minimized) {
         // client.minimized = false;
         workspace.activeWindow = client;
-        console.log(
-          "in current desktop, but minimized, activate now",
-          app_name,
-        );
+        // console.log(
+          // "in current desktop, but minimized, activate now",
+          // app_name,
+        // );
       } else {
-        console.log(
-          "in current desktop, but not minimized, minimized it now",
-          app_name,
-        );
+        // console.log(
+        //   "in current desktop, but not minimized, minimized it now",
+        //   app_name,
+        // );
         client.minimized = true;
       }
     }
