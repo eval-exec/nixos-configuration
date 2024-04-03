689cb5eabd36cc1ad83da366893c0ee284db76ef
Eval EXEC <execvy@gmail.com>
2024-04-03 15:37:34 +0800

    update

--- home.nix
+++ home.nix
@@ -428,7 +428,8 @@
     };
     emacs = {
       enable = true;
-      package = pkgs.emacs-git.override { withGTK3 = true; };
+      # package = pkgs.emacs-git.override { withGTK3 = true; };
+      package = pkgs.emacs-git;
 
       extraPackages = epkgs: [
         pkgs.emacsPackages.jinx
