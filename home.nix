21c9b9524ee8d14e51ae2af1062a4213c1b37a28
Eval EXEC <execvy@gmail.com>
2024-04-10 03:22:50 +0800

    nix flake update

--- home.nix
+++ home.nix
@@ -428,8 +428,8 @@
     };
     emacs = {
       enable = true;
-      # package = pkgs.emacs-git.override { withGTK3 = true; };
-      package = pkgs.emacs-git;
+      package = pkgs.emacs-git.override { withGTK3 = true; };
+      # package = pkgs.emacs-git;
 
       extraPackages = epkgs: [
         pkgs.emacsPackages.jinx
