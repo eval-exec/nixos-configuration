980ae20670c95284d3288a10e61345fc4f5527e6
Eval EXEC <execvy@gmail.com>
2024-04-18 22:10:46 +0800

    update emoji font

--- configuration.nix
+++ configuration.nix
@@ -422,40 +422,37 @@
       wqy_zenhei
     ];
     fontconfig = {
-      # localConf = ''
-      #   <?xml version="1.0"?>
-      #   <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-      #   <fontconfig>
-      #    <alias>
-      #      <family>sans-serif</family>
-      #      <prefer>
-      #        <family>Noto Sans</family>
-      #        <family>Noto Color Emoji</family>
-      #        <family>Noto Emoji</family>
-      #        <family>DejaVu Sans</family>
-      #      </prefer> 
-      #    </alias>
+      localConf = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+         <alias>
+           <family>sans-serif</family>
+           <prefer>
+             <family>Noto Sans</family>
+             <family>Noto Color Emoji</family>
+             <family>DejaVu Sans</family>
+           </prefer> 
+         </alias>
 
-      #    <alias>
-      #      <family>serif</family>
-      #      <prefer>
-      #        <family>Noto Serif</family>
-      #        <family>Noto Color Emoji</family>
-      #        <family>Noto Emoji</family>
-      #        <family>DejaVu Serif</family>
-      #      </prefer>
-      #    </alias>
+         <alias>
+           <family>serif</family>
+           <prefer>
+             <family>Noto Serif</family>
+             <family>Noto Color Emoji</family>
+             <family>DejaVu Serif</family>
+           </prefer>
+         </alias>
 
-      #    <alias>
-      #     <family>monospace</family>
-      #     <prefer>
-      #       <family>Noto Mono</family>
-      #       <family>Noto Color Emoji</family>
-      #       <family>Noto Emoji</family>
-      #      </prefer>
-      #    </alias>
-      #   </fontconfig>
-      # '';
+         <alias>
+          <family>monospace</family>
+          <prefer>
+            <family>Noto Mono</family>
+            <family>Noto Color Emoji</family>
+           </prefer>
+         </alias>
+        </fontconfig>
+      '';
       defaultFonts = {
         serif = [ "Serif" ];
         sansSerif = [ "Sans Serif" ];
