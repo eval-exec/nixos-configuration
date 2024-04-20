e66ed8b948387b30b47d266960fce956f1035262
Eval EXEC <execvy@gmail.com>
2024-04-21 00:05:53 +0800

    add tuna mirror

--- configuration.nix
+++ configuration.nix
@@ -576,6 +576,7 @@
       ];
 
       substituters = [
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
