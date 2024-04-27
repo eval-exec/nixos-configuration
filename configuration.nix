3be1a5945e71f2b01f05772bcad5641c8f79d386
Eval EXEC <execvy@gmail.com>
2024-04-27 13:35:47 +0800

    enable nvidia

--- configuration.nix
+++ configuration.nix
@@ -251,7 +251,7 @@
         };
       };
 
-      videoDrivers = [ "amdgpu" ];
+      videoDrivers = [ "nvidia" ];
       #   config = lib.mkAfter ''
       #     Section "Module"
       #         Load           "modesetting"
