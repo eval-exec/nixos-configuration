9f53bfde3d3e9ad0108291503b804baa68b6440c
Eval EXEC <execvy@gmail.com>
2025-06-29 14:31:40 +0800

    fmt

--- kwin_scripts/test.sh
+++ kwin_scripts/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -evx
- qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.cleanUp
+qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.cleanUp
 
- qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript /home/exec/Projects/github.com/eval-exec/nixos-configuration/kwin_scripts/toggle.js
- qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript /home/exec/Projects/github.com/eval-exec/nixos-configuration/kwin_scripts/toggle.js
- qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.start
- qdbus org.kde.KWin /KWin reconfigure
+qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript /home/exec/Projects/github.com/eval-exec/nixos-configuration/kwin_scripts/toggle.js
+qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript /home/exec/Projects/github.com/eval-exec/nixos-configuration/kwin_scripts/toggle.js
+qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.start
+qdbus org.kde.KWin /KWin reconfigure
