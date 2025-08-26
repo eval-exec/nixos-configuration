6fc2d5e05e7deb00772f467ac1b9d010d8da772a
Eval EXEC <execvy@gmail.com>
2025-08-26 14:42:40 +0800

    disable powermanagement

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -102,8 +102,6 @@
   # networking.interfaces.wlp59s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = "performance";
-  powerManagement.powertop.enable = false;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
   hardware.bluetooth = {
