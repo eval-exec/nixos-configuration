21dabb71675f2f32ed7ea9e0b43f107912d04be3
Eval EXEC <execvy@gmail.com>
2025-10-06 21:10:45 +0800

    gst

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -242,6 +242,8 @@
     gst_all_1.gst-plugins-good
     gst_all_1.gst-plugins-ugly
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
     gst_all_1.gstreamer
     gtest
     helix
