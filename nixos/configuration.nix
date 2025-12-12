10110f453f9230a8956241930763ee67f0ee51fd
Eval EXEC <execvy@gmail.com>
2025-12-12 16:27:35 +0800

    nix flake update, edit substitutes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,9 +95,10 @@
         ];
 
         substituters = [
-          "https://mirrors.ustc.edu.cn/nix-channels/store"
           "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-          # "https://mirror.sjtu.edu.cn/nix-channels/store"
+          "https://mirrors.ustc.edu.cn/nix-channels/store"
+          "https://mirror.nju.edu.cn/nix-channels/store"
+          "https://mirror.sjtu.edu.cn/nix-channels/store"
           "https://cache.nixos.org/"
           "https://nix-community.cachix.org"
           "https://devenv.cachix.org"
