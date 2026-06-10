378b73e5b3553e1b92b3cf9c0bc3beeee890b32f
Eval Exec <execvy@gmail.com>
2026-06-10 10:59:55 +0800

    emacs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1040,8 +1040,8 @@
           Type = "simple";
           Restart = "on-failure";
           RestartSec = 1;
-          # ExecStart = "${pkgs.nix}/bin/nix-shell /home/exec/.config/emacs/default.nix --run /home/exec/Projects/github.com/emacs-mirror/build/bin/emacs";
-          ExecStart = "${pkgs.emacs-git}/bin/emacs";
+          ExecStart = "${pkgs.nix}/bin/nix-shell /home/exec/.config/emacs/default.nix --run /home/exec/Projects/github.com/emacs-mirror/build/bin/emacs";
+          # ExecStart = "${pkgs.emacs-git}/bin/emacs";
           StandardOutput = "journal";
           StandardError = "journal";
         };
